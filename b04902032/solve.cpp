830921fc517b7012b8b3c1150017afa8310fef8c
yanger <yanger@haha.cc>
2018-10-24 14:15:23 +0800

    First version. Can only solve first two cases in small.in

--- b04902032/solve.cpp
+++ b04902032/solve.cpp
@@ -13,102 +13,49 @@ const int N = 55;
 
 const int dx[] = {1, 0, -1, 0};
 const int dy[] = {0, 1, 0, -1};
-
-int n, m;
-char mat[N][N];
-vector<PII> goal
-
-struct board {
-	int eval;
-	PII player;
-	vector<PII> box;
-	bool operator<(const board b) const {
-		return eval > b.eval;
-	}
-} cur;
+const char alph[] = "drul";
 
 bool solid(const char c) {
 	return c=='#' or c=='$';
 }
 
-bool valid(const int mv) {
-	PII tmp = MP(cur.player.F+dx[mv], cur.player.S+dy[mv]);
-	unzip();
-	if(mat[tmp.F][tmp.S] == '#')
-		return false;
-	if(mat[tmp.F][tmp.S] == '$' and solid(mat[tmp.F+dx[mv]][tmp.S+dy[mv]]))
-		return false;
-	return true;
-}
 
-void move(const int mv) {
-	PII tmp = MP(cur.player.F+dx[mv], cur.player.S+dy[mv]);
-	if(mat[tmp.F][tmp.S] == '$') {
-		mat[tmp.F+dx[mv]][tmp.S+dy[mv]] = '$';
-		for(int i=0; i<SZ(cur.box); i++) {
-			if(cur.box[i] == tmp) {
-				cur.box[i] = MP(tmp.F+dx[mv], tmp.S+dy[mv]);
-				break;
-			}
+
+class board {
+public:
+	static int n, m;
+	static char mat[N][N];
+	static vector<PII> goal;
+
+	int eval;
+	PII player;
+	set<PII> box;
+	vector<int> steps;
+
+	void update() {
+		int num = SZ(goal);
+		/*
+		flow.init(num*2+2);
+		for(int i=0; i<num; i++) {
+			int j=0;
+			for(PII b : box)
+				flow.add(1+i, num+1+(j++), abs(goal[i].F-b.F)+abs(goal[i].S-b.S));
 		}
+		*/
+		for(int i=0; i<num; i++)
+			flow.add(0, 1+i, INF);
+		for(int i=0; i<num; i++)
+			flow.add(num+1+i, 2*num+1, INF);
+		eval = flow.max_flow();
 	}
-	mat[cur.player.F][cur.player.S] = '-';
-	cur.player = tmp;
-	mat[tmp.F][tmp.S] = '@';
-}
-// undo mv
-void reverse(const int mv) {
-	int rv = (mv+2)&3;
-	PII tmp = MP(cur.player.F+dx[rv], cur.player.S+dy[rv]);
-	PII tmp2 = MP(cur.player.F+dx[mv], cur.player.S+dy[mv]);
-	assert(!solid(mat[tmp.F][tmp.S]) and mat[cur.F][cur.S] == '@');
-	if(mat[tmp2.F][tmp2.S] == '$') {
-		mat[cur.player.F][cur.player.S] = '$';
-		for(int i=0; i<SZ(cur.box); i++) {
-			if(cur.box[i] == tmp2) {
-				cur.box[i] = cur.player;
-				break;
-			}
-		}
-		mat[cur.F+dx[mv]][cur.S+dy[mv]] = '-';
-	}
-	mat[tmp.F][tmp.S] = '@';
-	cur.player=tmp;
-}
 
-int eval() {
-	int num = SZ(goal);
-	flow.init(num*2+2);
-	for(int i=0; i<num; i++)
-		for(int j=0; j<num; j++)
-			flow.add(1+i, num+1+j, abs(goal[i].F-box[j].F)+abs(goal[i].S-box[j].S));
-	for(int i=0; i<num; i++)
-		flow.add(0, 1+i, INF);
-	for(int i=0; i<num; i++)
-		flow.add(num+1+i, 2*num+1, INF);
-	return flow.max_flow();
-}
-
-board zip(int _eval = -1) {
-	board ret;
-	ret.player=player;
-	ret.box = box;
-	if(_eval == -1)
-		ret.eval = eval();
-	else
-		ret.eval = _eval;
-	return ret;
-}
-
-void init() {
-	goal.clear();
-	box.clear();
-}
-
-int main() {
-	while(scanf("%d%d", &n, &m) != EOF) {
+	static board init() {
+		board st;
+		st.player=MP(0, 0);
+		if(scanf("%d%d", &n, &m) == EOF)
+			return st;
 		fill(mat[0], mat[N], '#');
-		init();
+		goal.clear();
 		for(int i=1; i<=n; i++) {
 			char buf[N];
 			scanf("%s", buf);
@@ -119,23 +66,80 @@ int main() {
 					else if(buf[j-1] == '+') buf[j-1] = '@';
 					else buf[j-1] = '-';
 				}
-				if(buf[j-1] == '@')
-					player = MP(i, j);
+				if(buf[j-1] == '@') {
+					st.player = MP(i, j);
+					buf[j-1] = '-';
+				}
+				if(buf[j-1] == '$') {
+					st.box.insert(MP(i, j));
+					buf[j-1] = '-';
+				}
 				mat[i][j] = buf[j-1];
 			}
 		}
-		priority_queue<board> pque;
-		pque.push(zip());
+		st.update();
+		return st;
+	}
+
+	bool valid(const int mv) {
+		PII tmp = MP(player.F+dx[mv], player.S+dy[mv]);
+		PII ttmp = MP(tmp.F+dx[mv], tmp.S+dy[mv]);
+		if(mat[tmp.F][tmp.S] == '#')
+			return false;
+		if(box.count(tmp) and (mat[ttmp.F][ttmp.S] == '#' or box.count(ttmp)))
+			return false;
+		return true;
+	}
+
+	board move(const int mv) {
+		board ret = *this;
+		ret.steps.PB(mv);
+		PII tmp = MP(ret.player.F+dx[mv], ret.player.S+dy[mv]);
+		if(box.count(tmp)) {
+			ret.box.erase(tmp);
+			ret.box.insert(MP(tmp.F+dx[mv], tmp.S+dy[mv]));
+		}
+		ret.player = tmp;
+		ret.update();
+		return ret;
+	}
+
+	void print() {
+		printf("%d\n", SZ(steps));
+		for(int mv : steps)
+			putchar(alph[mv]);
+		putchar('\n');
+	}
+
+	bool operator<(const board b) const {
+		return SZ(steps)+eval > SZ(b.steps)+b.eval;
+	}
+};
+
+int board::n = 0, board::m=0;
+char board::mat[][N] = {};
+vector<PII> board::goal = vector<PII>();
+priority_queue<board> pque;
+
+int main() {
+	while(true) {
+		board start = board::init();
+		if(start.player == MP(0, 0))
+			break;
+		while(!pque.empty())
+			pque.pop();
+		pque.push(start);
 		while(!pque.empty()) {
 			board bd = pque.top();
-			pque.pop();
-			for(int i=0; i<4; i++) {
-				if(valid(i)) {
-					move(i);
-					pque.push(zip());
-					reverse(i);
-				}
+			//printf("%d %d\n", bd.player.F, bd.player.S);
+			if(bd.eval == 0) {
+				bd.print();
+				break;
 			}
+			pque.pop();
+			for(int i=0; i<4; i++)
+				if(bd.valid(i))
+					pque.push(bd.move(i));
 		}
 	}
 }
