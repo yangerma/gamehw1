2fa3c9a7d015ff5a6b91929db32d426c4f42def2
yanger <yanger@haha.cc>
2018-10-24 17:44:40 +0800

    Some bug fixed. small.in and first 8 cases in large.in solve.

--- b04902032/solve.cpp
+++ b04902032/solve.cpp
@@ -4,10 +4,10 @@ using namespace std;
 #define MP make_pair
 #define PB push_back
 */
-#include "dinic.cpp"
+#include "flow.cpp"
+typedef long long ull;
 typedef pair<int, int> PII;
-#define F first
-#define S second
+#define ALL(x) (x).begin(), (x).end()
 
 const int N = 55;
 
@@ -15,11 +15,7 @@ const int dx[] = {1, 0, -1, 0};
 const int dy[] = {0, 1, 0, -1};
 const char alph[] = "drul";
 
-bool solid(const char c) {
-	return c=='#' or c=='$';
-}
-
-
+unordered_map<ull, ull> vis;
 
 class board {
 public:
@@ -30,23 +26,40 @@ public:
 	int eval;
 	PII player;
 	set<PII> box;
-	vector<int> steps;
+	string steps;
+	
+	static int pos(PII p) {
+		return (p.F-1)*m + p.S-1;
+	}
+
+	ull hsh() {
+		ull ret=0;
+		for(PII p : box)
+			ret = ret*(n*m) + pos(p);
+		return ret;
+	}
 
 	void update() {
-		int num = SZ(goal);
 		/*
+		int num = SZ(goal);
 		flow.init(num*2+2);
 		for(int i=0; i<num; i++) {
 			int j=0;
 			for(PII b : box)
-				flow.add(1+i, num+1+(j++), abs(goal[i].F-b.F)+abs(goal[i].S-b.S));
+				flow.add(1+i, num+1+(j++), 1, abs(goal[i].F-b.F)+abs(goal[i].S-b.S));
 		}
+		for(int i=0; i<num; i++)
+			flow.add(0, 1+i, 1, 0);
+		for(int i=0; i<num; i++)
+			flow.add(num+1+i, 2*num+1, 1, 0);
+		eval = flow.max_flow().S;
 		*/
-		for(int i=0; i<num; i++)
-			flow.add(0, 1+i, INF);
-		for(int i=0; i<num; i++)
-			flow.add(num+1+i, 2*num+1, INF);
-		eval = flow.max_flow();
+		int j=0;
+		eval=0;
+		for(set<PII>::iterator ite=box.begin(); ite != box.end(); ite++) {
+			eval += abs(goal[j].F - ite->F) + abs(goal[j].S - ite->S);
+			j++;
+		}
 	}
 
 	static board init() {
@@ -77,6 +90,7 @@ public:
 				mat[i][j] = buf[j-1];
 			}
 		}
+		sort(ALL(goal));
 		st.update();
 		return st;
 	}
@@ -93,12 +107,15 @@ public:
 
 	board move(const int mv) {
 		board ret = *this;
-		ret.steps.PB(mv);
 		PII tmp = MP(ret.player.F+dx[mv], ret.player.S+dy[mv]);
-		if(box.count(tmp)) {
+		char ch = alph[mv];
+		bool flag=box.count(tmp);
+		if(flag) {
 			ret.box.erase(tmp);
 			ret.box.insert(MP(tmp.F+dx[mv], tmp.S+dy[mv]));
+			ch -= 32;
 		}
+		ret.steps.PB(ch);
 		ret.player = tmp;
 		ret.update();
 		return ret;
@@ -106,9 +123,24 @@ public:
 
 	void print() {
 		printf("%d\n", SZ(steps));
-		for(int mv : steps)
-			putchar(alph[mv]);
+		for(char ch : steps)
+			putchar(ch);
 		putchar('\n');
+	}
+
+	static void showmap() {
+		for(int i=0; i<=n; i++) {
+			for(int j=0; j<=m; j++)
+				putchar(mat[i][j]);
+			putchar('\n');
+		}
+	}
+	
+	bool done() {
+		ull tar=0;
+		for(PII p : goal)
+			tar = tar*(n*m) + pos(p);
+		return tar == hsh();
 	}
 
 	bool operator<(const board b) const {
@@ -120,26 +152,55 @@ int board::n = 0, board::m=0;
 char board::mat[][N] = {};
 vector<PII> board::goal = vector<PII>();
 priority_queue<board> pque;
+//queue<board> pque;
 
 int main() {
+	int lev=1;
 	while(true) {
 		board start = board::init();
 		if(start.player == MP(0, 0))
 			break;
+			/*
+		if(lev++ >= 100)
+			continue;
+
+*/
+		vis.clear();
 		while(!pque.empty())
 			pque.pop();
+
 		pque.push(start);
 		while(!pque.empty()) {
 			board bd = pque.top();
 			//printf("%d %d\n", bd.player.F, bd.player.S);
-			if(bd.eval == 0) {
+			if(bd.done()) {
 				bd.print();
 				break;
 			}
 			pque.pop();
-			for(int i=0; i<4; i++)
-				if(bd.valid(i))
-					pque.push(bd.move(i));
+			for(int i=0; i<4; i++) {
+				if(alph[(i+2)%4] == bd.steps.back())
+					continue;
+				if(bd.valid(i)) {
+					board nxt = bd.move(i);
+					assert(SZ(nxt.box) == SZ(board::goal));
+					ull val = nxt.hsh();
+					/*
+					printf("|| %d: %llu\n", i, val);
+					for(auto p : nxt.box)
+						printf("** %d %d\n", p.F, p.S);
+					*/
+					int pos = board::pos(nxt.player);
+					if(vis.count(val) and (vis[val]&(1ull<<pos)) != 0)
+						continue;
+					//printf("## %d: %llu\n", i, val);
+					pque.push(nxt);
+					if(!vis.count(val))
+						vis[val]=0;
+					vis[val] |= (1ull<<pos);
+				}
+			}
 		}
+		//puts("-----------");
 	}
 }
